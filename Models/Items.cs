6ab78850c79d75a1072f2d99ede93e0333532133
SV <suvithravani@gmail.com>
2023-08-06 18:11:47 +0800

    feat: table of items with basic add/delete functions

---
+++ Models/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace sales_order_creation.Models
+{
+    public class Items
+    {
+        public string name { get; set; } = string.Empty;
+        public string code { get; set; } = string.Empty;
+        public int qty { get; set; } = 0;
+        public decimal price { get; set; } = 0.00M;
+    }
+}
